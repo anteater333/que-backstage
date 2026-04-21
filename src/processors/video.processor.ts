dae7f8dc486a9f586907a40ce4099527042e98e3
Jihoon Lee <anteater1056@gmail.com>
2026-04-21 20:47:05 +0900

    feat: HLS 지원 영상 인코딩 프로세스 구현

--- src/processors/video.processor.ts
+++ src/processors/video.processor.ts
@@ -1,6 +1,7 @@
 import ffmpeg from "fluent-ffmpeg";
 import path from "node:path";
 import { CONFIG } from "../config";
+import { mkdir } from "node:fs/promises";
 
 /** 비디오 가공 파이프라인 실행 */
 export const processVideoPipeline = async (
@@ -18,12 +19,73 @@ export const processVideoPipeline = async (
     },
   );
   console.log("🥕 결과 :: ", results);
+
+  const resolutions = [
+    { name: "360p", width: 640, height: 360, bitrate: "800k" },
+    { name: "720p", width: 1280, height: 720, bitrate: "2500k" },
+    { name: "1080p", width: 1920, height: 1080, bitrate: "5000k" },
+  ];
+
+  console.log("영상 변환 테스트");
+  for (const res of resolutions) {
+    console.log(`🥕 ${res.name} 인코딩 시작 ---`);
+    await transcodeToHLS(rawFilePath, CONFIG.STORAGE.OUTPUT_PATH, { res });
+  }
 };
 
 type VideoOrientation = "landscape" | "portrait" | "square";
 /** TODO #0 필요 영상 메타데이터 추출 (ex. orientation 값 등) */
 
-/** TODO #1 해상도별 HLS 변환 */
+/**
+ * 해상도별 HLS 변환
+ **/
+export const transcodeToHLS = async (
+  input: string,
+  baseDir: string,
+  metadata: {
+    res: {
+      name: string;
+      width: number;
+      height: number;
+      bitrate: string;
+    };
+  },
+) => {
+  const resDir = path.join(baseDir, metadata.res.name);
+  await mkdir(resDir, { recursive: true });
+
+  return new Promise((resolve, reject) => {
+    ffmpeg(input)
+      .outputOptions([
+        // --- 비디오 설정 ---
+        "-preset veryfast",
+        "-profile:v high",
+        `-s ${metadata.res.width}x${metadata.res.height}`,
+        `-b:v ${metadata.res.bitrate}`,
+
+        // --- 오디오 설정 ---
+        "-c:a aac",
+        "-b:a 320k",
+        "-ar 48000",
+        "-ac 2",
+        "-af loudnorm=I=-16:TP=-1.5:LRA=11", // 오디오 필터(음량 평준화)
+
+        // --- HLS 설정 ---
+        "-hls_time 6",
+        "-hls_playlist_type vod",
+        `-hls_segment_filename ${resDir}/seg-%d.ts`,
+      ])
+      .output(`${resDir}/index.m3u8`)
+      .on("start", (commandLine) => console.log("[FFmpeg] 실행:", commandLine))
+      .on("progress", (progress) => {
+        const fixedPercent = Math.min(100, progress.percent || 0).toFixed(2);
+        console.log(`[FFmpeg - ${metadata.res.name}] 진행률: ${fixedPercent}%`);
+      })
+      .on("end", resolve)
+      .on("error", reject)
+      .run();
+  });
+};
 
 /**
  * 썸네일 추출
